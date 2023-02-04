4ae5694c09e7329e0cc91eb677cb05715f2ee77d
Seungkeun Lee <sngn.lee@samsung.com>
2023-02-04 21:18:32 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+let distance = 0
+led.enable(false)
+let 사람있음 = 0
+let distance2 = 200
+basic.forever(function () {
+    basic.pause(100)
+})
+basic.forever(function () {
+    if (사람있음 == 0 && distance2 < 30) {
+        music.playMelody("C5 B A G F E D C ", 120)
+        사람있음 = 1
+        serial.writeLine("\"사람있음..\"")
+    }
+    if (사람있음 == 1 && distance2 > 70) {
+        사람있음 = 0
+        music.playMelody("C D E F G A B C5 ", 120)
+    }
+})
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P2,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    basic.pause(100)
+    serial.writeValue("x", distance)
+    distance2 = distance
+})
